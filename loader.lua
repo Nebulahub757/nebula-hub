1cd3d931bbd9aa7ee165a7b857dd265e4395fdc4
Nebulahub757 <nebulahult0210@gmail.com>
2025-07-23 10:26:09 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,14 @@
+-- Nebula Hub Loader 🌌
+-- Este script carrega os módulos do hub
 
+local HttpGet = game:HttpGet
+
+-- Carrega os módulos do GitHub
+local gui = loadstring(HttpGet("https://raw.githubusercontent.com/Nebulahub757/nebula-hub/main/src/gui.lua"))()
+local autofarm = loadstring(HttpGet("https://raw.githubusercontent.com/Nebulahub757/nebula-hub/main/src/autofarm.lua"))()
+local drag = loadstring(HttpGet("https://raw.githubusercontent.com/Nebulahub757/nebula-hub/main/src/mobile_drag.lua"))()
+
+-- Inicializa a interface
+gui:CreateUI()
+drag:EnableDrag(gui.Frame)
+gui:BindAutoFarm(autofarm.Start)
